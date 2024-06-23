49723be52d93c2ccf37e914f83ecf57cf9cce6a0
Lordturkola <andreasdunca@yahoo.se>
2024-06-23 16:53:06 +0200

    updated vm to functioning state

--- app/src/main/java/com/example/tedf/ui/EnergyDrink/EnergyDrinkEntryViewModel.kt
+++ app/src/main/java/com/example/tedf/ui/EnergyDrink/EnergyDrinkEntryViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.tedf.ui.EnergyDrink
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.tedf.data.EnergyDrink
 import com.example.tedf.data.EnergyDrinkRepository
@@ -7,56 +10,8 @@ import java.text.NumberFormat
 
 class EnergyDrinkEntryViewModel(private val energyDrinkRepository: EnergyDrinkRepository) :
     ViewModel() {
-
-
-    // represents ui state for an energy drink item
-    data class EnergyDrinkUiState(
-        val energyDrinkDetails: EnergyDrinkDetails = EnergyDrinkDetails(),
-        val isEntryValid: Boolean = false,
-    )
-
-    // this describes it's contents or detail regarding the entity
-    data class EnergyDrinkDetails(
-        val id: Int = 0,
-        val name: String = "",
-        val price: Double = 0.0,
-        val rating: Int = -1,
-        val category: String = "",
-    )
-
-    fun EnergyDrinkDetails.toEnergyDrink(): EnergyDrink = EnergyDrink(
-        id = id,
-        name = name,
-        price = price.toDoubleOrNull() ?: 0.0,
-        rating = rating.toIntOrNull() ?: -1,
-        category = category,
-    )
-
-    fun EnergyDrink.formatedPrice(): String {
-        return NumberFormat.getCurrencyInstance().format(price)
-    }
-
-    fun EnergyDrink.toEnergyDrinkUiState(isEntryValid: Boolean = false): EnergyDrinkUiState =
-        EnergyDrinkUiState(
-            energyDrinkDetails = this.toEnergyDrinkDetails(),
-            isEntryValid = isEntryValid,
-        )
-
-    fun EnergyDrink.toEnergyDrinkDetails(): EnergyDrinkDetails = EnergyDrinkDetails(
-        id = id,
-        name = name,
-        price = price.toDoubleOrNull() ?: 0.0,
-        rating = rating.toIntOrNull() ?: -1,
-        category = category,
-    )
-
-    fun EnergyDrink.toEnergyDrinkDetails(): EnergyDrinkDetails = EnergyDrinkDetails(
-        id = id,
-        name = name,
-        price = price.toString(),
-        rating = rating.toString(),
-        category = category,
-    )
+    var energyDrinkUiState by mutableStateOf(EnergyDrinkUiState())
+        private set
 
     private fun validateInput(uiState: EnergyDrinkDetails = energyDrinkUiState.energyDrinkDetails): Boolean {
         return with(uiState) {
@@ -66,7 +21,57 @@ class EnergyDrinkEntryViewModel(private val energyDrinkRepository: EnergyDrinkRe
 
     suspend fun saveItem() {
         if (validateInput()) {
-            energyDrinkRepository.insertEnergyDrink(energyDrink = energyDrinkUiState.energyDrinkDetails.toItem())
+            energyDrinkRepository.insertEnergyDrink(energyDrink = energyDrinkUiState.energyDrinkDetails.toEnergyDrink())
         }
     }
+    fun updateUiState(energyDrinkDetails: EnergyDrinkDetails) {
+        energyDrinkUiState = EnergyDrinkUiState(
+            energyDrinkDetails = energyDrinkDetails,
+            isEntryValid = validateInput(energyDrinkDetails)
+        )
+    }
 }
+
+
+// represents ui state for an energy drink item
+data class EnergyDrinkUiState(
+    val energyDrinkDetails: EnergyDrinkDetails = EnergyDrinkDetails(),
+    val isEntryValid: Boolean = false,
+)
+
+// this describes it's contents or detail regarding the entity
+data class EnergyDrinkDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val price: String = "0.0",
+    val rating: String = "-1",
+    val category: String = "",
+)
+
+fun EnergyDrinkDetails.toEnergyDrink(): EnergyDrink = EnergyDrink(
+    id = id,
+    name = name,
+    price = price.toDoubleOrNull() ?: 0.0,
+    rating = rating.toIntOrNull() ?: -1,
+    category = category,
+)
+
+fun EnergyDrink.formatedPrice(): String {
+    return NumberFormat.getCurrencyInstance().format(price)
+}
+
+fun EnergyDrink.toEnergyDrinkUiState(isEntryValid: Boolean = false): EnergyDrinkUiState =
+    EnergyDrinkUiState(
+        energyDrinkDetails = this.toEnergyDrinkDetails(),
+        isEntryValid = isEntryValid,
+    )
+
+fun EnergyDrink.toEnergyDrinkDetails(): EnergyDrinkDetails = EnergyDrinkDetails(
+    id = id,
+    name = name,
+    price = price.toString(),
+    rating = rating.toString(),
+    category = category,
+)
+
+
